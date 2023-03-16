c68c8183d057c94d981f3b8c525f85ec73d30d48
Millat Hossaine <millathossain115@gmail.comm>
2023-03-17 04:03:11 +0600

    validation Handled

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,18 +1,22 @@
 document.getElementById("btn-withdraw").addEventListener("click", function () {
-    
+
     const newWithdrawAmount = getInputFieldById("withdraw-field");
     const previousWithdrawAmount = getElementValueById("withdraw-total");
-    if (newWithdrawAmount > previousWithdrawAmount) {
-        alert("Ato taka nai!!!");
-        return;
-    }
+
     const newWithdrawDispalyValue = newWithdrawAmount + previousWithdrawAmount;
 
-    setElementNewValueById("withdraw-total", newWithdrawDispalyValue);
 
     //New balance calculation
     const previousBalance = getElementValueById("balance-total");
-    const newBalance = previousBalance - newWithdrawAmount;
-    setElementNewValueById("balance-total", newBalance);
+    if (newWithdrawAmount > previousBalance) {
+        alert("Ato taka nai!!!");
+        return;
+    } else {
+        setElementNewValueById("withdraw-total", newWithdrawDispalyValue);
+
+        const newBalance = previousBalance - newWithdrawAmount;
+        setElementNewValueById("balance-total", newBalance);
+    }
+
 
 })
